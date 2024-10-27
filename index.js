f71f3be5b4c87133fe08086592992042c1c6e7f1
yonas790 <yoni2752@gmail.com>
2024-10-27 12:34:49 +0300

    upadating Dockerfile

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
